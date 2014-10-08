a426b617871292be94d7ea94865e0b1068906987
Axel Díaz <diaz.axelio@gmail.com>
2014-10-08 19:00:13 -0430

    Optimizando variables

--- update_fengoffice.sh
+++ update_fengoffice.sh
@@ -4,6 +4,11 @@ EXITO() {
 printf "\033[1;32m${1}\033[0m\n"
 }
 
+#VARIABLES
+DIR_APACHE='/var/www'
+DIR_RESPALDOS='$HOME/respaldos'
+REPOSITORIO='http://sourceforge.net/projects/opengoo/files/fengoffice'
+
 echo "Introduzca la versión a la que quiere actualizar"
 read VERSION
 EXITO "Ha seleccionado la version $VERSION"
@@ -13,19 +18,19 @@ mkdir -p /tmp/fengoffice/
 cd /tmp/fengoffice/
 
 EXITO "Descargando la version $VERSION"
-wget -c http://sourceforge.net/projects/opengoo/files/fengoffice/fengoffice_$VERSION/fengoffice_$VERSION.zip
+wget -c $REPOSITORIO/fengoffice_$VERSION/fengoffice_$VERSION.zip
 
 unzip fengoffice_$VERSION.zip
 rm fengoffice_$VERSION.zip
 
 EXITO "Iniciando respaldo de los directorios de Feng Office"
 sleep 1
-rsync -avzhP /var/www/fengoffice ~/respaldos/
+rsync -avzhP $DIR_APACHE/fengoffice $DIR_RESPALDOS
 EXITO "Respaldo finalizado"
 sleep 1
 
 EXITO "Iniciando respaldo de la base de datos"
-mkdir -p ~/respaldos/fengoffice/
+mkdir -p $DIR_RESPALDOS/fengoffice/
 
 echo "Por favor, ingrese el usuario de la base de datos"
 read USERDB
@@ -37,15 +42,15 @@ echo "Por favor, ingrese la contraseña de la base de datos"
 read PASSDB
 
 mysqldump -u $USERDB -p$PASSDB -d $DB > ~/respaldos/fengoffice/fengoffice.sql
-EXITO "Base de datos respaldada en $HOME/respaldos/fengoffice/fengoffice.sql"
+EXITO "Base de datos respaldada en $DIR_RESPALDOS/fengoffice/fengoffice.sql"
 sleep 1
 
 EXITO "Iniciando actualización de Feng Office a la versión $VERSION"
 sleep 1
-rsync -avzhP /tmp/fengoffice /var/www/
+rsync -avzhP /tmp/fengoffice $DIR_APACHE
 
 EXITO "Otorgando permisos a cache/ config/ tmp/ upload/"
 sleep 1
-cd /var/www/fengoffice/
+cd $DIR_APACHE/fengoffice/
 chmod -R o+w cache/ config/ tmp/ upload/
 EXITO "Actualización culminada"
