b2e45d3e560e1d9f2da9bdcf55d488e05ad2407b
Axel Díaz <diaz.axelio@gmail.com>
2014-10-08 18:47:06 -0430

    Añadiendo ingreso de versión

--- update_fengoffice.sh
+++ update_fengoffice.sh
@@ -1,8 +1,13 @@
+#!/bin/bash
+
 EXITO() {
 printf "\033[1;32m${1}\033[0m\n"
 }
 
-VERSION=2.7.1.1
+echo "Introduzca la versión a la que quiere actualizar"
+read VERSION
+EXITO "Ha seleccionado la version $VERSION"
+sleep 1
 
 mkdir -p /tmp/fengoffice/
 cd /tmp/fengoffice/
@@ -14,22 +19,24 @@ unzip fengoffice_$VERSION.zip
 rm fengoffice_$VERSION.zip
 
 EXITO "Iniciando respaldo de los directorios de Feng Office"
+sleep 1
 rsync -avzhP /var/www/fengoffice ~/respaldos/
-
 EXITO "Respaldo finalizado"
-echo " "
+sleep 1
 
 EXITO "Iniciando respaldo de la base de datos"
 mkdir -p ~/respaldos/fengoffice/
 
 mysqldump fengoffice > ~/respaldos/fengoffice/fengoffice.sql
 EXITO "Respaldo finalizado"
-echo " "
+sleep 1
 
 EXITO "Iniciando actualización de Feng Office a la versión $VERSION"
+sleep 1
 rsync -avzhP /tmp/fengoffice /var/www/
 
 EXITO "Otorgando permisos a cache/ config/ tmp/ upload/"
+sleep 1
 cd /var/www/fengoffice/
 chmod -R o+w cache/ config/ tmp/ upload/
 EXITO "Actualización culminada"
